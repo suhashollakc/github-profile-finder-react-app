7e69dd6c1ba940ee2f9d6709a3474cd4bf8d48d7
suhashollakc <suhashollakc@gmail.com>
2021-05-05 18:14:37 +0530

    pushing project repo to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users'
+import Search from './components/users/Search'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+
+state = {
+  users:[],
+  loading: false
+}
+
+// async componentDidMount(){ 
+
+//   this.setState({loading: true});
+
+//   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+//   this.setState(
+//     {
+//       users: res.data,loading:false
+//     }
+//   );
+// }
+searchUsers = async text =>{
+
+    this.setState({
+      loading:true
+    })
+  const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  this.setState(
+    {
+      users: res.data.items,loading:false
+    }
   );
+}
+  render(){
+    return (
+      <div className="App">
+       
+       <Navbar/>
+       <div class="container">
+         <Search searchUsers = {this.searchUsers}/>
+       <Users loading = {this.state.loading} users = {this.state.users} />
+       </div>
+
+      
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar= ({icon, title}) => {
+  
+        return (
+           <nav className="navbar bg-primary">
+               <h1>
+                   <i className={icon}/>
+                   {" "+title}</h1>
+           </nav>
+        );
+}
+
+Navbar.defaultProps = {
+    title: "GitFind",
+    icon: "fab fa-github"
+} 
+export default Navbar
